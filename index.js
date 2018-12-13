62d6a54f874bb0ae76e1c88a35b83c64a45d3998
Evan Procter <evanprocter@gmail.com>
2018-12-13 10:29:53 -0500

    set state and added switch statement

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('yay redux');
+// console.log('yay redux');
+const { createStore } = require('redux');
+
+// ==========================================
+// SETUP
+// ==========================================
+
+// #1 - Describe the note
+const defaultState = {
+    note: {
+        content: ''
+    }
+}
+
+
+// #2 - List out all the kinds.types of changes you could make to state
+/*
+- UPDATE_CONTENT
+*/
+const ACTION_UPDATE = {
+    type: 'ACTION_UPDATE',
+    // content: ''
+}
+
+// #3 - Write action creator functions
+// These are functions that use/return those descriptions
+// of changes you can make to state
+const updateContent = (content) => {
+    return {
+        ...ACTION_UPDATE,
+        content
+    }
+}
+
+// #4 - Write a reducer function that accepts stte and an action
+// and returns the next version of the state
+const not = (state=0, action) => {
+
+}
+
+
+
